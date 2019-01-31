22b67e1d6e2e3fadc60e76b54950b47425f32493
Anas Butt <anasahmad07@outlook.com>
2019-01-31 15:44:07 -0500

    added project secure-users

---
+++ secure_users/myapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from myapp import views
+#TEMPLATE URL
+
+app_name = 'myapp'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login')
+]
